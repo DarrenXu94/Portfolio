1ce486efbe7f8da3ebf94eb40d89e2144fc99ad2
Darren Xu <darrenxu1994@yahoo.com.au>
2019-08-11 12:16:48 +1000

    card css

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="project-card md-layout-item md-size-45 md-small-size-100">
     <md-card md-with-hover>
+      <md-card-media>
+        <img src="@/assets/hero.jpg" alt="hero" />
+      </md-card-media>
       <md-card-header>
         <div class="md-title">{{project.name}}</div>
       </md-card-header>
@@ -17,8 +20,19 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .project-card {
   margin: 10px;
+}
+.md-card {
+  border-radius: 10px;
+  cursor: default;
+  img {
+    max-height: 100px;
+    object-fit: cover;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    object-position: 50% 50%;
+  }
 }
 </style>
