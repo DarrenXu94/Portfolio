e7a2551e781c167587e95eef914c7ab6d13bd1b3
Darren Xu <darrenxu1994@yahoo.com.au>
2019-08-14 21:59:35 +1000

    fixing about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,14 @@
   <div class="projects md-layout">
     <div class="md-layout-item main-raised md-size-80 md-small-size-95 md-elevation-3">
       <div class="container">
-        <div class="about">
-          <div class="experience">
+        <div class="about md-layout">
+          <div class="experience md-layout-item md-size-80 md-small-size-95">
             <div class="experience-container">
               <div v-for="exp in experience" :key="exp.id" class="experience-card md-layout">
-                <div class="logo-container md-layout-item md-size-20">
+                <div class="logo-container md-layout-item md-size-20 md-small-size-100">
                   <img :src="exp.img" alt />
                 </div>
-                <div class="details-container md-layout-item md-size-80">
+                <div class="details-container md-layout-item md-size-80 md-small-size-100">
                   <h3>{{exp.company}}</h3>
                   <h4>{{exp.title}}</h4>
                   <h5>{{exp.dates}}</h5>
@@ -43,48 +43,81 @@ export default {
   width: 100%;
   background: none;
 }
+
 .experience {
+  padding: 15px;
+
+  img {
+    width: 96px;
+    height: 96px;
+    border-radius: 50%;
+    border: 0.3rem solid #05222f;
+    z-index: 2;
+    background-color: #fff;
+  }
   .experience-container {
-    // display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    max-width: 768px;
-    margin: auto;
+    background: white;
+    border-radius: 5px;
     box-shadow: 0px 2px 40px 1px rgb(111, 105, 107);
   }
+
   .experience-card {
-    color: #333;
-    flex-direction: row;
-    position: relative;
-    background-color: #fff;
-    box-sizing: border-box;
     padding: 1.5rem 1.5rem 0;
-    width: 768px;
-    border-left: 1px solid #dcd9d9;
-    border-right: 1px solid #dcd9d9;
-    align-items: center;
-    img {
-      width: 96px;
-      height: 96px;
-      border-radius: 50%;
-      border: 0.3rem solid #05222f;
-      z-index: 2;
-      background-color: #fff;
-    }
   }
-  .details-container {
+
+  .logo-container {
     display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    padding: 0 0 1.5rem 1.5rem;
-    border-bottom: 1px solid #dcd9d9;
-    text-align: left;
+    align-items: center;
+    justify-content: center;
   }
-  h3,
-  h4,
-  h5 {
-    margin: 0 0 0.6rem;
+
+  .details-container {
+    border-bottom: 1px solid #dcd9d9;
   }
 }
+
+// .experience {
+//   .experience-container {
+//     // display: flex;
+//     justify-content: center;
+//     flex-direction: column;
+//     align-items: center;
+//     max-width: 768px;
+//     margin: auto;
+//     box-shadow: 0px 2px 40px 1px rgb(111, 105, 107);
+//   }
+//   .experience-card {
+//     color: #333;
+//     flex-direction: row;
+//     position: relative;
+//     background-color: #fff;
+//     box-sizing: border-box;
+//     padding: 1.5rem 1.5rem 0;
+//     width: 768px;
+//     border-left: 1px solid #dcd9d9;
+//     border-right: 1px solid #dcd9d9;
+//     align-items: center;
+//     img {
+//       width: 96px;
+//       height: 96px;
+//       border-radius: 50%;
+//       border: 0.3rem solid #05222f;
+//       z-index: 2;
+//       background-color: #fff;
+//     }
+//   }
+//   .details-container {
+//     display: flex;
+//     flex-direction: column;
+//     flex-grow: 1;
+//     padding: 0 0 1.5rem 1.5rem;
+//     border-bottom: 1px solid #dcd9d9;
+//     text-align: left;
+//   }
+//   h3,
+//   h4,
+//   h5 {
+//     margin: 0 0 0.6rem;
+//   }
+// }
 </style>
